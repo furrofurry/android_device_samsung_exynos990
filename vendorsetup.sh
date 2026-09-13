6964ed89523f5d4604e42dec4dc8cc9bd25c0d0f
Racks Rask <82312571+furrofurry@users.noreply.github.com>
2026-09-13 13:44:29 +0000

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,19 +6,16 @@ export FOX_BUILD_DEVICE="z3s"
 export FOX_VANILLA_BUILD=1
 export OF_DISABLE_MIUI_SPECIFIC_FEATURES=1
 
-# Dynamic partitions
+# Dynamic partitions / fastbootd
 export OF_ENABLE_ALL_PARTITION_TOOLS=1
 
-# Samsung Keymaster
+# Samsung Keymaster 4.0
 export OF_DEFAULT_KEYMASTER_VERSION="4.0"
 
-# z3s is NOT an A/B device
+# z3s is non-A/B
 export FOX_AB_DEVICE=0
 
-# Android 16/API 36:
-# DO NOT enable this for the first build.
-#
-# If recovery boots and the remaining decryption failure is specifically
-# related to API 36 libc++/keystore2, test this later:
+# Android 16 / API 36 experimental support.
+# Leave disabled for the first build.
 #
 # export FOX_ADD_API_V36_PREBUILTS=2
